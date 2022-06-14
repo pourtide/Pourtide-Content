529c45ef7d8afad066c5941b4a4523e8b17d4d70
DarktideLegend <apourman@protonmail.com>
2022-06-14 13:10:26 -0700

    updated: made bloodhunter oil unusable

---
+++ sql/weenies/15408 Bloodhunter Oil.sql
@@ -0,0 +1,48 @@
+DELETE FROM `weenie` WHERE `class_Id` = 15408;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (15408, 'bloodhunteroil', 44, '2022-06-14 20:00:17') /* CraftTool */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (15408,   1,   67108864) /* ItemType - CraftAlchemyIntermediate */
+     , (15408,   3,         76) /* PaletteTemplate - Orange */
+     , (15408,   5,         15) /* EncumbranceVal */
+     , (15408,   8,          5) /* Mass */
+     , (15408,   9,          0) /* ValidLocations - None */
+     , (15408,  11,        100) /* MaxStackSize */
+     , (15408,  12,          1) /* StackSize */
+     , (15408,  13,         15) /* StackUnitEncumbrance */
+     , (15408,  14,          5) /* StackUnitMass */
+     , (15408,  15,         10) /* StackUnitValue */
+     , (15408,  16,          1) /* ItemUseable - No */
+     , (15408,  19,         10) /* Value */
+     , (15408,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (15408,  94,  134217857) /* TargetType - MeleeWeapon, Misc, CraftFletchingIntermediate */
+     , (15408, 150,        103) /* HookPlacement - Hook */
+     , (15408, 151,         11) /* HookType - Floor, Wall, Yard */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (15408,  69, False) /* IsSellable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (15408,   1, 'Bloodhunter Oil') /* Name */
+     , (15408,  14, 'This item is used in fletching.') /* Use */
+     , (15408,  20, 'Vials of Bloodhunter Oil') /* PluralName */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (15408,   1,   33555967) /* Setup */
+     , (15408,   3,  536870932) /* SoundTable */
+     , (15408,   6,   67111919) /* PaletteBase */
+     , (15408,   7,  268435815) /* ClothingBase */
+     , (15408,   8,  100672647) /* Icon */
+     , (15408,  22,  872415275) /* PhysicsEffectTable */;
+
+/* Lifestoned Changelog:
+{
+  "LastModified": "2022-06-14T13:01:27.6959813-07:00",
+  "ModifiedBy": "pourman",
+  "Changelog": [],
+  "UserChangeSummary": "test",
+  "IsDone": false
+}
+*/
